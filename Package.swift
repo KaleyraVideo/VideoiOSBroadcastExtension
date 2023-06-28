505cc22122a2ba7e4ed5163ba8f0cf9e18095c0a
Marco Brescianini <marco.brescianini@kaleyra.com>
2023-06-28 17:06:47 +0200

    Update package.swift extracting version and checksum variables

--- Package.swift
+++ Package.swift
@@ -2,6 +2,9 @@
 
 import PackageDescription
 
+let version = "1.2.0"
+let checksum = "4d0196ec3e46d4961b723341c9b8ab47ced62720be225532063f7ad1d7277d86"
+
 let package = Package(
     name: "BandyerBroadcastExtension",
     platforms: [
@@ -15,7 +18,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "BandyerBroadcastExtension",
-            url: "https://static.bandyer.com/corporate/iOS/releases/broadcastextension/1.2.0/xcframework/BandyerBroadcastExtension.framework.zip",
-            checksum: "4d0196ec3e46d4961b723341c9b8ab47ced62720be225532063f7ad1d7277d86")
+            url: "https://static.bandyer.com/corporate/iOS/releases/broadcastextension/\(version)/xcframework/BandyerBroadcastExtension.framework.zip",
+            checksum: checksum)
     ]
 )
